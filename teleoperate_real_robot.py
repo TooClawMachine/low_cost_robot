e683ba70b0c978f43377d9e7d4eaf4daab2ae242
Clay <radiantclay@gmail.com>
2024-11-07 21:05:27 -0500

    local adaption

--- teleoperate_real_robot.py
+++ teleoperate_real_robot.py
@@ -1,10 +1,10 @@
 from robot import Robot
 from dynamixel import Dynamixel
 
-leader_dynamixel = Dynamixel.Config(baudrate=1_000_000, device_name='/dev/tty.usbmodem57380045631').instantiate()
-follower_dynamixel = Dynamixel.Config(baudrate=1_000_000, device_name='/dev/tty.usbserial-FT8ISNO8').instantiate()
-follower = Robot(follower_dynamixel, servo_ids=[1, 2, 3, 4, 6, 7])
-leader = Robot(leader_dynamixel, servo_ids=[1, 2, 3, 4, 6, 7])
+leader_dynamixel = Dynamixel.Config(baudrate=1_000_000, device_name='COM5').instantiate()
+follower_dynamixel = Dynamixel.Config(baudrate=1_000_000, device_name='COM6').instantiate()
+follower = Robot(follower_dynamixel, servo_ids=[1, 2, 3, 4, 5, 6])
+leader = Robot(leader_dynamixel, servo_ids=[1, 2, 3, 4, 5, 6])
 leader.set_trigger_torque()
 
 
