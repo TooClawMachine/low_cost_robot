6c2f2fac651c21ccc76412650a5d272dcc93a3e9
Clay <radiantclay@gmail.com>
2024-12-04 12:32:34 -0500

    launch both scripts at the same time

--- launch.bat
+++ launch.bat
@@ -1,1 +1,2 @@
-python -m uvicorn main:app --reload --host 0.0.0.0 --port 8000
+start python -m uvicorn main:app --reload --host 0.0.0.0 --port 8000
+start python -m uvicorn camera_server:app --reload --host 0.0.0.0 --port 8001
